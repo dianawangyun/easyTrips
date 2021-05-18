7a7d04b97c2001cd7fded7914448419bf94f119d
wangyun <dianawangyun@gmail.com>
2021-05-17 22:26:57 -0500

    db

--- db.js
+++ db.js
@@ -3,8 +3,15 @@
 const { Client } = require("pg");
 const { getDatabaseUri } = require("./config");
 
+// const db = new Client({
+//     connectionString: getDatabaseUri(),
+// });
+
 const db = new Client({
     connectionString: getDatabaseUri(),
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
 
 try {
